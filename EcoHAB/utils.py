3884836ef2ec1416111d084a4c1272142673cf1c
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-11-30 13:32:38 +0100

    Take into account last slash of the path

--- EcoHAB/utils.py
+++ EcoHAB/utils.py
@@ -1,5 +1,9 @@
+import os
+
 def results_path(path):
     head, tail = os.path.split(path)
+    if tail == '':
+        head, tail = os.path.split(head)
     head, tail2 = os.path.split(head)
     head = os.path.join(head,'Results_'+tail2)
     return os.path.join(head,tail)
