50d4dbae3c87e3e360911a9d5f1e6db3f69f1e1a
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2017-11-30 13:20:09 +0100

    Utility functions

---
+++ EcoHAB/utils.py
@@ -0,0 +1,5 @@
+def parse_path(path):
+    head, tail = os.path.split(path)
+    head, tail2 = os.path.split(head)
+    head = os.path.join(head,'Results_'+tail2)
+    return os.path.join(head,tail)
