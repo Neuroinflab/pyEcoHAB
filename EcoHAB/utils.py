959b5e2bcecf0aec362e438a082eed404c2a5919
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-01-12 10:55:32 +0100

    Make results directory name

--- EcoHAB/utils.py
+++ EcoHAB/utils.py
@@ -6,3 +6,17 @@ def check_directory(directory,subdirectory):
         os.makedirs(new_path)
     return new_path
         
+def results_path(path):
+    head, tail = os.path.split(path)
+    if tail == '':
+        head, tail = os.path.split(head)
+    head, tail2 = os.path.split(head)
+    head = os.path.join(head,'Results_'+tail2)
+    return os.path.join(head,tail)
+
+def make_figure(title):
+    fig = plt.figure(figsize=(12,12))
+    ax = fig.add_subplot(111, aspect='equal')
+    fig.suptitle('%s'%(title), fontsize=14, fontweight='bold')
+    return fig, ax
+    
