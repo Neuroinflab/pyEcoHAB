e5ba03fe00bb69e8994ae93bb79c94b3866b078a
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-05-28 08:18:54 +0200

    Generate prefix for filenames with results

--- EcoHAB/utils.py
+++ EcoHAB/utils.py
@@ -19,4 +19,46 @@ def make_figure(title):
     ax = fig.add_subplot(111, aspect='equal')
     fig.suptitle('%s'%(title), fontsize=14, fontweight='bold')
     return fig, ax
-    
+
+def make_prefix(path):
+    """
+    Read-in info.txt and make a prefix for all files for results.
+    Parameters
+    ----------
+    path: str
+    """
+    key_list = [
+        'genotype',
+        'sex',
+        'type of experiment',
+        'date of experiment',
+        'social odor',
+        'no social odor',
+    ]
+
+    fname = os.path.join(path, 'info.txt')
+
+    try:
+        f = open(fname)
+    except IOError:
+        return ''
+
+    info_dict = {}
+    for line in f:
+        key, info = line.split(':')
+        info = info.strip()
+        info = info.replace(' ', '_')
+        info_dict[key] = info
+    prefix = ''
+    for key in key_list:
+        if key not in info_dict:
+            continue
+        if key == 'social odor' or key == 'non-social odor':
+            if info_dict[key] == 'none' or info_dict[key] == 'None':
+                key = key.replace(' ', '_')
+                prefix += '_no_' + key.replace(' ', '_') + '_'
+            else:
+                prefix += key.replace(' ', '_') + '_' + info_dict[key] + '_'
+        else:
+            prefix += info_dict[key] + '_'
+    return prefix
