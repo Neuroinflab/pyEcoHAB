c25b6c9c4c8bcd279ef54caab59a8272d4f53696
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2018-08-27 15:43:55 +0200

    Starter of a module

---
+++ EcoHAB/__init__.py
@@ -0,0 +1,5 @@
+import os
+ecohab_loc = os.path.dirname(os.path.abspath(__file__))
+sample_data_path = os.path.join(ecohab_loc, '..', 'data')
+
+from .EcoHab import EcoHabData, EcoHabSessions, EcoHabSessions9states
