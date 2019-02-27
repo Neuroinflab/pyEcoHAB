474555c1542ec8badf19c2eee6eb578976a2ad6a
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-02-27 16:32:33 +0100

    Check if finding change indices works

--- EcoHAB/tests/test_utility_functions.py
+++ EcoHAB/tests/test_utility_functions.py
@@ -3,45 +3,52 @@
 from __future__ import print_function, division, absolute_import
 from EcoHAB import utility_functions as uf
 import unittest
+import numpy as np
 
 class TestInPipe(unittest.TestCase):
     def test_pipe_1_2_clockwise(self):
-        self.assertEqual(uf.in_tube(1, 2), True)
+        self.assertTrue(uf.in_tube(1, 2))
     def test_pipe_2_1_counterclockwise(self):
-        self.assertEqual(uf.in_tube(2, 1), True)
+        self.assertTrue(uf.in_tube(2, 1))
     def test_pipe_2_3_clockwise(self):
-        self.assertEqual(uf.in_tube(2, 3), False)
+        self.assertFalse(uf.in_tube(2, 3))
     def test_pipe_3_2_counterclockwise(self):
-        self.assertEqual(uf.in_tube(3, 2), False)
+        self.assertFalse(uf.in_tube(3, 2))
     def test_pipe_7_8_clockwise(self):
-        self.assertEqual(uf.in_tube(7, 8), True)
+        self.assertTrue(uf.in_tube(7, 8))
     def test_pipe_8_7_counterclockwise(self):
-        self.assertEqual(uf.in_tube(8, 7), True)
+        self.assertTrue(uf.in_tube(8, 7))
     def test_pipe_1_8_clockwise(self):
-        self.assertEqual(uf.in_tube(1, 8), False)
+        self.assertFalse(uf.in_tube(1, 8))
     def test_pipe_8_1_counterclockwise(self):
-        self.assertEqual(uf.in_tube(8, 1), False)
+        self.assertFalse(uf.in_tube(8, 1))
 
 class TestInChambers(unittest.TestCase):
     def test_pipe_1_2_clockwise(self):
-        self.assertEqual(uf.in_chamber(1, 2), False)
+        self.assertFalse(uf.in_chamber(1, 2))
     def test_pipe_2_1_counterclockwise(self):
-        self.assertEqual(uf.in_chamber(2, 1), False)
+        self.assertFalse(uf.in_chamber(2, 1))
     def test_pipe_2_3_clockwise(self):
-        self.assertEqual(uf.in_chamber(2, 3), True)
+        self.assertTrue(uf.in_chamber(2, 3))
     def test_pipe_3_2_counterclockwise(self):
-        self.assertEqual(uf.in_chamber(3, 2), True)
+        self.assertTrue(uf.in_chamber(3, 2))
     def test_pipe_7_8_clockwise(self):
-        self.assertEqual(uf.in_chamber(7, 8), False)
+        self.assertFalse(uf.in_chamber(7, 8))
     def test_pipe_8_7_counterclockwise(self):
-        self.assertEqual(uf.in_chamber(8, 7), False)
+        self.assertFalse(uf.in_chamber(8, 7))
     def test_pipe_1_8_clockwise(self):
-        self.assertEqual(uf.in_chamber(1, 8), True)
+        self.assertTrue(uf.in_chamber(1, 8))
     def test_pipe_8_1_counterclockwise(self):
-        self.assertEqual(uf.in_chamber(8, 1), True)
+        self.assertTrue(uf.in_chamber(8, 1))
     
+class TestChangeState(unittest.TestCase):
+    def test_check_no_change(self):
+        self.assertEqual(len(uf.change_state([1, 1, 1, 1,])), 0)
+    def test_check_change_1(self):
+        self.assertFalse(len(uf.change_state([1, 1, 1, 2])) == 0)
+    def test_check_change_2(self):
+        self.assertTrue(uf.change_state([1, 1, 1, 2]), np.array([2], dtype=int))
         
-    
 
 if __name__ == '__main__':
     unittest.main()
