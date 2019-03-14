dab5e5fe9509b7e774ee7cc8861902260f5b0be6
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-03-14 16:43:22 +0100

    Make figures with mice locations to check get_state function

--- EcoHAB/tube_dominance_experiment_with_2_cages.py
+++ EcoHAB/tube_dominance_experiment_with_2_cages.py
@@ -8,9 +8,18 @@ import numpy as np
 from write_to_file import save_single_histograms, write_csv_rasters
 from plotfunctions import single_in_cohort_soc_plot, make_RasterPlot, single_heat_map
 from numba import jit
+import matplotlib.pyplot as plt
 mouse_attention_span = 10  # sec
 nbins = 10
 homepath = os.path.expanduser("~/")
+opposite_antenna_dict = {1: 2,
+                    2: 1}
+dt = 0.05
+color_list = ['indianred', 'darkred', 'salmon',
+              'darkolivegreen', 'forestgreen', 'darkslategrey',
+              'darkslateblue', 'lightskyblue', 'navy',
+              'mediumpurple', 'grey', 'gold',
+              'orange', 'saddlebrown', 'magenta']
 def get_states(ehd, cf, mouse, dt = 0.05):
     """
     0 -- home cage, 1 -- pipe, 2 -- cage with stimulus
@@ -66,7 +75,12 @@ def check_mouse1_defending(m1_states, m1_times, antennas2, times2, home_cage_ant
 
 
 if __name__ == '__main__':
-    datasets = ['EcoHAB_data_November/Social structure males 02.03/']
+    datasets = [#'EcoHAB_data_November/social_structure_swiss_webster_ctrl_05.02.18',]
+                 'EcoHAB_data_November/Social structure males 02.03/',
+                # 'EcoHAB_data_November/social_dominance_swiss_webster_dominant_remove_12.02.18',
+                # 'EcoHAB_data_November/social_structure_16.01',
+                # 'EcoHAB_data_November/social_structure_19.01.18_rep_II',
+                 ]
     for new_path in datasets:
         path = os.path.join(homepath, new_path)
         prefix = utils.make_prefix(path)
@@ -92,6 +106,33 @@ if __name__ == '__main__':
         res_dir = utils.results_path(path)
         cf1 = ExperimentConfigFile(path)
         home_cage_antenna = ehd1.get_home_cage_antenna()
+        states = {}
+        for mouse1 in ehd1.mice:
+            states[mouse1] = get_states(ehd1, cf1, mouse1, dt)
+        T_START, T_END = cf1.gettime('ALL')
+        for phase in cf1.sections():
+            if phase == 'ALL':
+                continue
+            t_start, t_end = cf1.gettime(phase)
+            print(phase, t_start, t_end)
+                        
+            idx_start = int((t_start-T_START)/dt)
+            idx_end =  int((t_end-T_START)/dt)
+            time = np.arange(t_start, t_end, dt)
+            ehd1.mask_data(t_start, t_end)
+            if phase != 'ALL':
+                fig = plt.figure()
+                ax = fig.add_subplot(1, 1, 1)
+                ax.set_title(phase)
+                for i, mouse in enumerate(ehd1.mice):
+                    print(mouse)
+                    print(ehd1.getantennas(mouse))
+
+                    ax.plot(time/1000, 10*states[mouse][idx_start:idx_end]+0.1*i, color=color_list[i], label=mouse)
+                ax.legend()
+                ax.set_xlabel(time)
+            ehd1.unmask_data()
+
         utils.evaluate_whole_experiment(ehd1, cf1, res_dir, prefix,
                                         tube_dominance_2_cages,
                                         'tube_dominance',
