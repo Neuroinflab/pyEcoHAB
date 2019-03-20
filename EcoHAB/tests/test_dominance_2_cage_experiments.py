bedf096b664f039863300421874e35e5ea2f2fc7
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-03-20 13:12:41 +0100

    Test if pipe is detected with 2 readings of the same antenna in 2 sec

--- EcoHAB/tests/test_dominance_2_cage_experiments.py
+++ EcoHAB/tests/test_dominance_2_cage_experiments.py
@@ -7,42 +7,42 @@ import numpy as np
 class TestGetStates(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        dt = 0.1
+        cls.dt = 0.1
         cls.t_start = 600.0
         cls.t_end = 800.0
         cls.home_antenna_1 = 3
         cls.home_antenna_2 = 4
 
-        cls.antennas_1 = [3, 4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3]
-        cls.times_1 = [641.083, #3
-                      642.135, #4
-                      675.134, #4
-                      675.869, #3
-                      681.127, #3
-                      681.734, #4
-                      692.744, #4
-                      693.207, #3
-                      701.82,  #3
-                      702.603, #4
-                      703.499, #4
-                      703.961, #3
-                      723.136, #3
-                      725.633, #4
-                      734.133, #4
-                      734.945, #3
-                      783.411]#3
-        cls.out_1 = dom.get_states_mouse(cls.antennas_1,
-                                         cls.times_1,
+        cls.antennas = [3, 4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3, 4, 4, 3, 3]
+        cls.times = [641.083, #3 #0
+                      642.135, #4 #1
+                      675.134, #4 #2
+                      675.869, #3 #3
+                      681.127, #3 #4
+                      681.734, #4 #5
+                      692.744, #4 #6
+                      693.207, #3 #7
+                      701.82,  #3 #8
+                      702.603, #4 #9
+                      703.499, #4 #10
+                      703.961, #3 #11
+                      723.136, #3 #12
+                      725.633, #4 #13
+                      734.133, #4 #14
+                      734.945, #3 #15
+                      783.411] #3 #16
+        cls.out_1 = dom.get_states_mouse(cls.antennas,
+                                         cls.times,
                                          cls.t_start,
                                          cls.t_end,
                                          cls.home_antenna_1,
-                                         dt)
-        cls.out_2 = dom.get_states_mouse(cls.antennas_1,
-                                         cls.times_1,
+                                         cls.dt)
+        cls.out_2 = dom.get_states_mouse(cls.antennas,
+                                         cls.times,
                                          cls.t_start,
                                          cls.t_end,
                                          cls.home_antenna_2,
-                                         dt)
+                                         cls.dt)
         # for i, x in enumerate(cls.out_2):
         #     print(i, x)
     def test_same_length(self):
@@ -71,6 +71,15 @@ class TestGetStates(unittest.TestCase):
     def test_different_home_for_home_antenna_2(self):
         self.assertTrue(np.any(self.out_2 == 0))
 
+    def test_threshold_1(self):
+        timestamp_1 = int(round((self.times[9] - self.t_start)/self.dt))
+        timestamp_2 = int(round((self.times[9] - self.t_start)/self.dt))
+        self.assertTrue(np.all(self.out_1[timestamp_1:timestamp_2] == 1))
+
+    def test_threshold_1(self):
+        timestamp_1 = int(round((self.times[9] - self.t_start)/self.dt))
+        timestamp_2 = int(round((self.times[9] - self.t_start)/self.dt))
+        self.assertTrue(np.all(self.out_2[timestamp_1:timestamp_2] == 1))
 
 if __name__ == '__main__':
     unittest.main()
