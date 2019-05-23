2815c5c0b4df3e3f9e802c0bf84c7fb7959a23e6
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-05-23 14:33:58 +0200

    Calculate visits and their durations in an interval divided into bins

--- EcoHAB/cage_visits.py
+++ EcoHAB/cage_visits.py
@@ -36,34 +36,36 @@ def visits_and_durations(intervals, t_start, t_stop):
     return visits, durations
 
 
+def visits_and_durations_phase(intervals, time_start,
+                           time_end, binsize):
+    length = int(np.ceil((time_end - time_start)/binsize))
+    visits = np.zeros(length)
+    durations = np.zeros(length)
+    time = time_start
+    for i in range(length):
+        t_start = time_start + i*binsize
+        t_end = time_start + (i+1)*binsize
+        visits[i], durations[i] = visits_and_durations(intervals,
+                                                       t_start, t_end)
     return visits, durations
 
 
-def get_one_phase(ehd, cf, phase, address, binsize):
-    t_start, t_end = cf.gettime(phase)
-    mice = ehd.mice
-    length = int(np.ceil((t_end - t_start)/binsize/3600))
-    visits = np.zeros(len(mice), length)
-    durations = np.zeros(len(mice), length)
-    data = utils.get_ehs_data(ehs, mouse, t_start, t_end)
-    for address in range(1, 5):
-        intervals = utils.get_intervals(data, address)
-    
-
-
-def get_cage_visits(ehs, cf,
-                    res_dir=None,
-                    prefix=None,
-                    which_phases=None,
-                    remove_mouse=None):
-    if prefix is None:
-        prefix = ehd.prefix
-    if res_dir is None:
-        res_dir = ehd.res_dir
-    mice = utils.get_mice(ehs.mice, remove_mouse)
-    add_info_mice = utils.add_info_mice_filename(remove_mouse)
-    full_results = np.zeros((len(phases), len(mice), len(mice)))
-    full_results_exp = np.zeros((len(phases), len(mice), len(mice)))
-    for idx_phase, phase in enumerate(phases):
-        if get_data:
-            data = utils.prepare_data(ehs, mice, cf.gettime(phase))
+# def get_cage_visits(ehs, cf,
+#                     res_dir=None,
+#                     prefix=None,
+#                     which_phases=None,
+#                     remove_mouse=None):
+#     if prefix is None:
+#         prefix = ehd.prefix
+#     if res_dir is None:
+#         res_dir = ehd.res_dir
+#     mice = utils.get_mice(ehs.mice, remove_mouse)
+#     add_info_mice = utils.add_info_mice_filename(remove_mouse)
+#     full_results = np.zeros((len(phases), len(mice), len(mice)))
+#     full_results_exp = np.zeros((len(phases), len(mice), len(mice)))
+#Get intervals for the whole experiment and then loop?
+#     for idx_phase, phase in enumerate(phases):
+#             t_start, t_end = cf.gettime(phase)
+#             data = utils.get_ehs_data(ehs, mouse, t_start, t_end)
+#             for address in range(1, 5):
+#                 intervals = utils.get_intervals(data, address)
--- EcoHAB/tests/test_cage_visits.py
+++ EcoHAB/tests/test_cage_visits.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division, absolute_import
 from EcoHAB import cage_visits as cv
+import numpy as np
 import unittest
 
 class TestVisitsAndDurations(unittest.TestCase):
@@ -48,6 +49,81 @@ class TestVisitsAndDurations(unittest.TestCase):
         v, d = cv.visits_and_durations(intervals, t_start, t_stop)
         self.assertEqual(v, 1)
         self.assertEqual(d, 90)
+
+
+class TestVisitsDurationsPhase(unittest.TestCase):
+    def test_one_bin_test_intervals_only_in_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 5], [12, 15], [18, 20], [40, 70], [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_phase(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([4]))
+        out_durations = np.array_equal(d, np.array([45]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_one_bin_test_intervals_starting_before_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_phase(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([5]))
+        out_durations = np.array_equal(d, np.array([46]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_one_bin_test_intervals_ending_after_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 4], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [95, 130]]
+        v, d = cv.visits_and_durations_phase(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([5]))
+        out_durations = np.array_equal(d, np.array([50]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_one_bin_test_intervals(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [95, 130]]
+        v, d = cv.visits_and_durations_phase(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([6]))
+        out_durations = np.array_equal(d, np.array([51]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+
+    def test_more_bin_test_intervals_only_in_the_bin(self):
+        t_start = 0
+        t_stop = 100
+        intervals = [[1, 5], [12, 15], [18, 20], [40, 70], [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_phase(intervals, t_start, t_stop, 10)
+        out_visits = np.array_equal(v, np.array([1, 2, 0, 0, 1,
+                                                 1, 1, 0, 1, 0]))
+        out_durations = np.array_equal(d, np.array([4, 5, 0, 0, 10,
+                                                    10, 10, 0, 10, 0]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_more_bin_test_intervals_starting_before_the_bin(self):
+        t_start = 0
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_phase(intervals, t_start, t_stop, 10)
+        out_visits = np.array_equal(v, np.array([1, 3, 0, 0,
+                                                 1, 1, 1, 0, 1, 0]))
+        out_durations = np.array_equal(d, np.array([9, 6, 0, 0, 10,
+                                                    10, 10, 0, 10, 0]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
 
 if __name__ == '__main__':
     unittest.main()
