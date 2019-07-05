7ca66787b479aee96096a5def5bab18abed5e6d8
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-07-05 16:32:00 +0200

    Add forgotten functionality

--- EcoHAB/__init__.py
+++ EcoHAB/__init__.py
@@ -8,3 +8,4 @@ from .analiza_friends import get_in_cohort_sociability, get_mouse_alone
 from .cage_visits import get_visits
 from .tube_dominance import get_tube_dominance
 from .dominance_in_2_cages import get_subversion_evaluation, get_tube_dominance_2_cages, get_visits_to_stimulus_cage
+from .mouse_speed import get_following
