190d45770a7f7cdd30c8a340c6f76590f262f6c4
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-07-11 13:16:56 +0200

    Updates to functions for saving results from master

--- EcoHAB/write_to_file.py
+++ EcoHAB/write_to_file.py
@@ -1,35 +1,42 @@
+from __future__ import division, print_function, absolute_import
 import os
-import utils
 import numpy as np
+from . import utility_functions as utils
+
 
 def write_single_chamber(f, header, heads, address, mice, phases, time, data_stim ):
-    for j,h in enumerate(heads):
+    longest = 0
+    for j, h in enumerate(heads):
         f.write(h%address)
         f.write(header+'\n')
-        
         for i, mouse in enumerate(mice):
-            
-            lines = [mouse for i in data_stim[j][phases[0]][mouse]]
-            
+            longest = 0
             for phase in phases:
-                for k,t in enumerate(time[phase]):
+                if len(data_stim[j][phase][mouse])> longest:
+                    longest = len(data_stim[j][phase][mouse])
+            lines = [mouse for i in range(longest)]
+
+            for phase in phases:
+                for k, t in enumerate(time[phase]):
                     if phase == phases[0]:
                         lines[k] += ';%3.2f'%(t/3600)
+
                     lines[k] += ';'+str(data_stim[j][phase][mouse][k])
                                                                 
             for line in lines:
                 f.write(line+'\n')
 
-def save_data_cvs(data, fname, path, which, headers):
-    new_path = os.path.join(path, 'time_in_chambers')
+def save_data_cvs(data, fname, path, which, headers,
+                  target_dir="time_in_chambers"):
+    new_path = os.path.join(path, target_dir)
     if not os.path.exists(new_path):
+        print(new_path)
         os.makedirs(new_path)
+
     fname = os.path.join(new_path, fname)
-    print(fname)
     f = open(fname,'w')
     phases = data['phases']
     mice = data['mice']
-    
     header = 'mouse;\"time [h]\"'
     for phase in phases:
         header += ';\"'+phase+"\""
@@ -44,6 +51,7 @@ def save_single_histograms(result, fname, mice, phase, main_directory, directory
     directory = utils.check_directory(main_directory, new_name)
     if isinstance(additional_info, str):
         fname =  os.path.join(directory, '%s_%s_%s_%s.csv'% (fname, prefix, phase, additional_info))
+        
     else:
         fname =  os.path.join(directory, '%s_%s_%s.csv'% (fname, prefix, phase))
     try:
@@ -76,7 +84,7 @@ def write_csv_rasters(mice, phases, output, directory, dirname, fname):
         
     header += '\n'
     f.write(header)
-    new_output, pairs = make_table_of_pairs(output, phases, mice)
+    new_output, pairs = utils.make_table_of_pairs(output, phases, mice)
     for i, pair in enumerate(pairs):
         f.write(pair)
         for j in range(len(phases)):
@@ -94,19 +102,21 @@ def write_csv_tables(results, phases, mice, main_directory, dirname, fname, pref
     except IOError:
         print('Could not write to ', fname)
         return
-    print(fname)
+    
     phase_pairs = [phases[2*i]+(len(mice)+2)*';'+ phases[2*i+1]+'\n' for i in range(len(phases)//2)]
     new_results = [(results[2*i], results[2*i+1])for i in range(len(phases)//2)]
-
-    mice_header = ';'
     
-    for i in range(2):
+    mice_header = ';'
+    mice_len = 2
+    if len(phases) == 1:
+        mice_len = 1
+    for i in range(mice_len):
         for mouse in mice:
             mice_header += mouse + ';'
         if not i:
             mice_header += ';;'
     mice_header += '\n'
-    
+   
     if len(phases) % 2:
         phase_pairs.append((phases[-1]+'\n'))
         new_results.append((results[-1],))
@@ -128,21 +138,20 @@ def write_csv_tables(results, phases, mice, main_directory, dirname, fname, pref
         f.write('\n')
     f.close()
 
-def write_csv_alone(alone, phases, mice, main_directory, prefix):
-    directory = utils.check_directory(main_directory, 'mouse_alone')
-    fname =  os.path.join(directory, 'mouse_alone_%s.csv' % prefix)
+def write_csv_alone(alone, phases, mice, main_directory, prefix, labels=["1", "2", "3", "4"],  header='Mice alone in chamber %s\n', fname='mouse_alone_%s.csv', directory="mouse_alone"):
+    directory = utils.check_directory(main_directory, directory)
+    fname =  os.path.join(directory, fname % prefix)
     try:
         f = open(fname, 'w')
     except IOError:
         print('Could not write to ', fname)
         return
-    header = 'Mice alone in chamber %d\n'
     phases_header = 'Mouse;'
     for phase in phases:
         phases_header += phase + ';'
     phases_header += '\n'
-    for i in range(1, 5):
-        f.write(header % i)
+    for i in range(1, alone.shape[0]+1):
+        f.write(header % labels[i-1])
         f.write(phases_header)
         for j, mouse in enumerate(mice):
             f.write(mouse+';')
@@ -150,18 +159,30 @@ def write_csv_alone(alone, phases, mice, main_directory, prefix):
                 f.write(str(alone[i-1, j, k])+';')
             f.write('\n')
     f.close()
-    
-def make_table_of_pairs(FAM, phases, mice):
-    new_shape = (len(mice)*(len(mice)-1)//2, len(phases))
-    output = np.zeros(new_shape)
-    pair_labels = utils.list_of_pairs(mice)
-    for i, phase in enumerate(phases):
-        l = 0
-        for j, mouse in enumerate(mice):
-            for k in range(j+1, len(mice)):
-                output[l,i] = FAM[i,j,k]
-                l += 1
 
-    return output, pair_labels
-
-
+def write_interpair_intervals(results, main_directory,
+                              directory, fname, prefix,
+                              additional_info=None):
+    new_name = os.path.join(main_directory, 'data')
+    directory = utils.check_directory(directory, new_name)
+    if isinstance(additional_info, str):
+        fname =  os.path.join(directory, '%s_%s_%s.csv'% (fname,
+                                                          prefix,
+                                                          additional_info))
+    else:
+        fname =  os.path.join(directory, '%s_%s.csv'% (fname,
+                                                       prefix))
+    try:
+        f = open(fname, 'w')
+    except IOError:
+        print('Could not write to file', fname)
+        return None
+    print(fname)
+    f.write("followed mouse, following mouse, intervals\n")
+    keys = sorted(results.keys())
+    for key in keys:
+        mouse1, mouse2 = key.split('_')
+        f.write("%s,%s," % (mouse1, mouse2))
+        for interval in results[key]:
+            f.write("%f," % interval)
+        f.write("\n")
