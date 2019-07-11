6ecc41a93faca82f5133be3bb8410fdf05c4faf6
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-07-11 13:36:26 +0200

    Tests for methods

---
+++ EcoHAB/tests/test_cage_visits.py
@@ -0,0 +1,129 @@
+from __future__ import print_function, division, absolute_import
+from EcoHAB import cage_visits as cv
+import numpy as np
+import unittest
+
+class TestVisitsAndDurations(unittest.TestCase):
+    def test_intervals_only_in_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 5], [12, 15], [18, 20], [40, 70], [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations(intervals, t_start, t_stop)
+        self.assertEqual(v, 4)
+        self.assertEqual(d, 45)
+
+    def test_intervals_starting_before_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations(intervals, t_start, t_stop)
+        self.assertEqual(v, 4)
+        self.assertEqual(d, 45)
+
+    def test_intervals_ending_after_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 4], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [95, 130]]
+        v, d = cv.visits_and_durations(intervals, t_start, t_stop)
+        self.assertEqual(v, 5)
+        self.assertEqual(d, 80)
+
+    def test_intervals(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [95, 130]]
+        v, d = cv.visits_and_durations(intervals, t_start, t_stop)
+        self.assertEqual(v, 5)
+        self.assertEqual(d, 80)
+
+    def test_one_loonger_interval(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 110]]
+        v, d = cv.visits_and_durations(intervals, t_start, t_stop)
+        self.assertEqual(v, 0)
+        self.assertEqual(d, 0)
+
+
+class TestVisitsDurationsPhase(unittest.TestCase):
+    def test_one_bin_test_intervals_only_in_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 5], [12, 15], [18, 20], [40, 70], [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_bins(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([4]))
+        out_durations = np.array_equal(d, np.array([45]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_one_bin_test_intervals_starting_before_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_bins(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([4]))
+        out_durations = np.array_equal(d, np.array([45]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_one_bin_test_intervals_ending_after_the_bin(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 4], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [95, 130]]
+        v, d = cv.visits_and_durations_bins(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([5]))
+        out_durations = np.array_equal(d, np.array([80]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_one_bin_test_intervals(self):
+        t_start = 10
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [95, 130]]
+        v, d = cv.visits_and_durations_bins(intervals, t_start, t_stop, 90)
+        out_visits = np.array_equal(v, np.array([5]))
+        out_durations = np.array_equal(d, np.array([80]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+
+    def test_more_bin_test_intervals_only_in_the_bin(self):
+        t_start = 0
+        t_stop = 100
+        intervals = [[1, 5], [12, 15], [18, 20], [40, 70], [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_bins(intervals, t_start, t_stop, 10)
+        out_visits = np.array_equal(v, np.array([1, 2, 0, 0, 1,
+                                                 0, 0, 0, 1, 0]))
+        out_durations = np.array_equal(d, np.array([4, 5, 0, 0, 30,
+                                                    0, 0, 0, 10, 0]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+    def test_more_bin_test_intervals_starting_before_the_bin(self):
+        t_start = 0
+        t_stop = 100
+        intervals = [[1, 11], [12, 15],
+                     [18, 20], [40, 70],
+                     [80, 90], [110, 130]]
+        v, d = cv.visits_and_durations_bins(intervals, t_start, t_stop, 10)
+        out_visits = np.array_equal(v, np.array([1, 2, 0, 0,
+                                                 1, 0, 0, 0, 1, 0]))
+        out_durations = np.array_equal(d, np.array([10, 5, 0, 0, 30,
+                                                    0, 0, 0, 10, 0]))
+        self.assertTrue(out_visits)
+        self.assertTrue(out_durations)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ EcoHAB/tests/test_mouse_speed.py
@@ -0,0 +1,135 @@
+from __future__ import print_function, division, absolute_import
+from EcoHAB import mouse_speed as ms
+import unittest
+
+try:
+    basestring
+except NameError:
+    basestring = str
+
+class TestCheck2ndMouse(unittest.TestCase):
+    def test_following(self):
+        antenna1 = 1
+        antenna2 = 2
+        t1 = 15
+        threshold = 3
+        antennas2 = [8, 1, 2, 3, 4, 5]
+        times2 = [10, 16, 19, 19.5, 22, 25]
+        out = ms.check_2nd_mouse(antenna1, antenna2, t1, threshold,
+                                 antennas2, times2)
+        self.assertEqual(out, 1)
+
+    def test_taking_over(self):
+        antenna1 = 1
+        antenna2 = 2
+        t1 = 15
+        threshold = 3
+        antennas2 = [8, 1, 2, 3, 4, 5]
+        times2 = [10, 16, 18, 19.5, 22, 25]
+        out = ms.check_2nd_mouse(antenna1, antenna2, t1, threshold,
+                                 antennas2, times2)
+        self.assertEqual(out, 0)
+
+    def test_not_following(self):
+        antenna1 = 1
+        antenna2 = 2
+        t1 = 15
+        threshold = 3
+        antennas2 = [8, 1, 2, 3, 4, 5, 6]
+        times2 = [16, 19, 19.5, 22, 25]
+        out = ms.check_2nd_mouse(antenna1, antenna2, t1, threshold,
+                                 antennas2, times2)
+        self.assertEqual(out, 0)
+
+
+class TestFollowing2ndMouseInPipe(unittest.TestCase):
+    def test_following_1(self):
+        antennas1 = [1, 2]
+        times1 = [15, 16.5]
+        antennas2 = [8, 1, 2, 3, 4, 5]
+        times2 = [10, 16, 19, 19.5, 22, 25]
+        out, intervals = ms.following_2_mice_in_pipe(antennas1, times1,
+                                                     antennas2, times2)
+        self.assertEqual(out, 1)
+        self.assertEqual(intervals, [3])
+
+    def test_not_following(self):
+        antennas1 = [1, 2]
+        times1 = [15, 16.5]
+        antennas2 = [8, 1, 2, 3, 4, 5]
+        times2 = [10, 16, 19, 19.5, 22, 25]
+        out, intervals = ms.following_2_mice_in_pipe(antennas2, times2,
+                                                     antennas1, times1)
+        self.assertEqual(out, 0)
+        self.assertEqual(intervals, [])
+
+    def test_following_more(self):
+        antennas1 = [1, 2, 3, 4, 5]
+        times1 = [15, 16.5, 19, 20, 21]
+        antennas2 = [8, 1, 2, 3, 4, 5]
+        times2 = [10, 16, 19, 19.5, 22, 25]
+        out, intervals = ms.following_2_mice_in_pipe(antennas1, times1,
+                                                     antennas2, times2)
+        self.assertEqual(out, 2)
+        self.assertEqual(intervals, [3, 2.5])
+
+    def test_not_following_more(self):
+        antennas1 = [1, 2, 3, 4, 5]
+        times1 = [15, 16.5, 19, 20, 21]
+        antennas2 = [8, 1, 2, 3, 4, 5]
+        times2 = [10, 16, 19, 19.5, 22, 25]
+        out, intervals = ms.following_2_mice_in_pipe(antennas2, times2,
+                                                     antennas1, times1)
+        self.assertEqual(out, 0)
+        self.assertEqual(intervals, [])
+
+
+class TestCalculateExpectedFollowings(unittest.TestCase):
+    def test_summing(self):
+        wm1 = {'1': 2, '2':5}
+        fm2 = {'1':.5, '2':.5}
+        out = ms.calculate_expected_followings(wm1, fm2)
+        self.assertTrue(out, 3)
+
+
+class TestFrequencyMouseInTube(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.period = 16.
+        cls.antennas = [1, 1, 2, 3, 4, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8]
+        cls.times = [0, 1, 1.5, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        cls.freq, cls.window = ms.frequency_mouse_in_tube(cls.times,
+                                                          cls.antennas,
+                                                          cls.period)
+    def test_same_keys(self):
+        self.assertEqual(self.freq.keys(), self.window.keys())
+
+    def test_all_keys(self):
+        self.assertEqual(set(self.freq.keys()), set([3, 7, 11, 15]))
+
+    def test_freq_12(self):
+        self.assertEqual(self.freq[3], 3/16)
+
+    def test_freq_34(self):
+        self.assertEqual(self.freq[7], 3/16)
+
+    def test_freq_56(self):
+        self.assertEqual(self.freq[11], 1/16)
+
+    def test_freq_78(self):
+        self.assertEqual(self.freq[15], 1/16)
+
+    def test_window_12(self):
+        self.assertEqual(self.window[3], 2.5)
+
+    def test_window_34(self):
+        self.assertEqual(self.window[7], 3)
+
+    def test_window_56(self):
+        self.assertEqual(self.window[11], 1)
+
+    def test_window_78(self):
+        self.assertEqual(self.window[15], 1)
+
+if __name__ == '__main__':
+    unittest.main()
