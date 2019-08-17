2f19bfa849e11c7bf39e37406f3c0a8da240f552
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-08-17 16:28:38 +0200

    Import function calculating visit durations for the whole experiment

--- pyEcoHAB/__init__.py
+++ pyEcoHAB/__init__.py
@@ -5,7 +5,7 @@ sample_data_path = os.path.join(ecohab_loc, '..', 'data')
 from .EcoHab import EcoHabData, EcoHabSessions
 from .ExperimentConfigFile import ExperimentConfigFile
 from .analiza_friends import get_in_cohort_sociability, get_mouse_alone
-from .cage_visits import get_visits
+from .cage_visits import get_all_visits
 from .tube_dominance import get_tube_dominance
 from .dominance_in_2_cages import get_subversion_evaluation, get_tube_dominance_2_cages, get_visits_to_stimulus_cage
 from .mouse_speed import get_following
