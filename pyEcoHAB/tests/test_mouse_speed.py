f3ba4c94e62bcf35f63c2aba5fe6361a13ecba83
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-09-06 17:04:24 +0200

    Update tests (direction changes)

--- pyEcoHAB/tests/test_mouse_speed.py
+++ pyEcoHAB/tests/test_mouse_speed.py
@@ -105,31 +105,58 @@ class TestFrequencyMouseInTube(unittest.TestCase):
         self.assertEqual(self.freq.keys(), self.window.keys())
 
     def test_all_keys(self):
-        self.assertEqual(set(self.freq.keys()), set([3, 7, 11, 15]))
+        self.assertEqual(set(self.freq.keys()), set(['12', '21', '34',
+                                                     '43', '56', '65',
+                                                     '78', '87']))
 
     def test_freq_12(self):
-        self.assertEqual(self.freq[3], 3/16)
+        self.assertEqual(self.freq['12'], 2/16)
+
+    def test_freq_21(self):
+        self.assertEqual(self.freq['21'], 1/16)
 
     def test_freq_34(self):
-        self.assertEqual(self.freq[7], 3/16)
+        self.assertEqual(self.freq['34'], 2/16)
+
+    def test_freq_43(self):
+        self.assertEqual(self.freq['43'], 1/16)
 
     def test_freq_56(self):
-        self.assertEqual(self.freq[11], 1/16)
+        self.assertEqual(self.freq['56'], 1/16)
+
+    def test_freq_65(self):
+        self.assertEqual(self.freq['65'], 0)
 
     def test_freq_78(self):
-        self.assertEqual(self.freq[15], 1/16)
+        self.assertEqual(self.freq['78'], 1/16)
+
+    def test_freq_87(self):
+        self.assertEqual(self.freq['87'], 0)
+
 
     def test_window_12(self):
-        self.assertEqual(self.window[3], 2.5)
+        self.assertEqual(self.window['12'], 1.5)
+
+    def test_window_21(self):
+        self.assertEqual(self.window['21'], 1)
 
     def test_window_34(self):
-        self.assertEqual(self.window[7], 3)
+        self.assertEqual(self.window['34'], 2)
+
+    def test_window_43(self):
+        self.assertEqual(self.window['43'], 1)
 
     def test_window_56(self):
-        self.assertEqual(self.window[11], 1)
+        self.assertEqual(self.window['56'], 1)
+
+    def test_window_65(self):
+        self.assertEqual(self.window['65'], 0)
 
     def test_window_78(self):
-        self.assertEqual(self.window[15], 1)
+        self.assertEqual(self.window['78'], 1)
+
+    def test_window_87(self):
+        self.assertEqual(self.window['87'], 0)
 
 if __name__ == '__main__':
     unittest.main()
