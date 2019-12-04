75fd9d8b3ba9b72b61ac5979e58b50e41207f33d
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2019-12-04 13:35:27 +0100

    Move Results directory to data directory per Alicja's specification

--- pyEcoHAB/utility_functions.py
+++ pyEcoHAB/utility_functions.py
@@ -52,12 +52,7 @@ def check_directory(directory,subdirectory):
 
 
 def results_path(path):
-    head, tail = os.path.split(path)
-    if tail == '':
-        head, tail = os.path.split(head)
-    head, tail2 = os.path.split(head)
-    head = os.path.join(head, 'Results_'+tail2)
-    return os.path.join(head, tail, 'Results')
+    return os.path.join(path, 'Results')
 
 
 def make_figure(title):
