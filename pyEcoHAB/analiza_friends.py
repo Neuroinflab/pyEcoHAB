60898c5d6dc05e463b1f3b9034bef15484d3c685
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.gov.pl>
2020-03-06 11:06:02 +0100

    Typo

--- pyEcoHAB/analiza_friends.py
+++ pyEcoHAB/analiza_friends.py
@@ -85,7 +85,7 @@ def mouse_alone(data_mice, address):
 
 def make_solitude_output(addresses, mice):
     output = OrderedDict()
-    for address in adressses:
+    for address in adresses:
         output[address] = OrderedDict()
         for mouse in mice:
             output[address][mouse] = OrderedDict()
