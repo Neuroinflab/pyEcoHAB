92245244bc6555ebf11ee1011732e4c8e70f024b
Asia Jędrzejewska-Szmek <asia@in.waw.pl>
2020-03-17 15:20:39 +0100

    Bud of the module

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='pyEcoHAB',
+     version='0.0.0.1',
+     author="Joanna Jędrzejewska-Szmek, Jan Mąka, Szymon Łęski",
+     author_email="j.jedrzejewska-szmek@nencki.edu.pl",
+     description="Reading-in and analysis of behavioral data acquired by the EcoHAB system",
+     url="https://github.com/Neuroinflab/pyEcoHAB",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: BSD",
+         "Operating System :: OS Independent",
+     ],
+ )
