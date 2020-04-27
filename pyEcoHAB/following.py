64cdf1bae0c137efd6a1b641ff999f61b5a0ef23
Asia Jędrzejewska-Szmek <asia@in.waw.pl>
2020-04-27 13:39:34 +0200

    Save measured leading

--- pyEcoHAB/following.py
+++ pyEcoHAB/following.py
@@ -308,8 +308,8 @@ def get_following(ehd, cf, N, res_dir=None, prefix=None,
     else:
         method = "mean_N_%d" % N
     fname = 'following_%s_%s' % (method, add_info_mice)
-    fname_ = 'following_%s_%s%s.csv' % (method, prefix,
-                                         add_info_mice)
+    fname_ = 'following_%s%s.csv' % (prefix, add_info_mice)
+    fname_rev_ = 'leading_%s%s.csv' % (prefix, add_info_mice)
     fname_beg = 'following_excess'
     fname_rev = 'leading_excess'
     fname_exp = '%s_%s_%s%s.csv' % (fname_beg,
@@ -451,6 +451,14 @@ def get_following(ehd, cf, N, res_dir=None, prefix=None,
                       'following/raster_plots',
                       fname_,
                       symmetric=False)
+    write_csv_rasters(ehd.mice,
+                      phases,
+                      following,
+                      res_dir,
+                      'following/raster_plots',
+                      fname_rev_,
+                      symmetric=False,
+                      reverse_order=True)
     write_csv_rasters(ehd.mice,
                       phases,
                       (following-following_exp),
