abf1f2fb4244fc1d3d213701ee5f0b569b4544dd
Joanna Jędrzejewska-Szmek <asia@in.waw.pl>
2021-07-26 12:01:40 +0200

    Forgot to fix tests

--- pyEcoHAB/incohort_sociability.py
+++ pyEcoHAB/incohort_sociability.py
@@ -405,7 +405,11 @@ def get_incohort_sociability(ecohab_data, timeline, binsize, res_dir="",
                             title="Excess in-cohort sociability",
                             symmetrical=True)
 
-        write_sum_data(excess_time_per_mouse, 'excess_incohort_sociability_per_mouse', mice, bin_labels, all_phases,
-                       res_dir, out_dir_hist, prefix, additional_info=add_info_mice, delimiter=delimiter, bool_bins=True)
+            write_sum_data(excess_time_per_mouse,
+                           'excess_incohort_sociability_per_mouse',
+                           mice, bin_labels, all_phases,
+                           res_dir, out_dir_hist, prefix,
+                           additional_info=add_info_mice,
+                           delimiter=delimiter, bool_bins=True)
 
     return full_results, full_results_exp
