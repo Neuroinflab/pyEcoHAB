f10dff09aac6d5c12588e84dfedf1263ed965af5
Joanna Jędrzejewska-Szmek <j.jedrzejewska-szmek@nencki.edu.pl>
2021-10-04 11:17:50 +0200

    Try this out

--- pyEcoHAB/__init__.py
+++ pyEcoHAB/__init__.py
@@ -1,6 +1,6 @@
 import os
 ecohab_loc = os.path.dirname(os.path.abspath(__file__))
-data_path = os.path.join(ecohab_loc, '..', 'data')
+data_path = os.path.join(ecohab_loc, 'data')
 sample_data = os.path.join(data_path, "BALB_VPA_data_cohort_1")
 
 from .Loader import Loader, Merger
